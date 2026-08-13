13e83bec625cbe2aaee5091ef91aee5feb2aa0a7
ervan code <ervanoffice6@gmail.com>
2026-08-13 11:03:37 +0700

    Add files via upload

--- app/service-worker.js
+++ app/service-worker.js
@@ -3,7 +3,7 @@
 // di-intercept sama sekali, jadi selalu langsung ke network (tidak relevan di-cache & tidak
 // boleh diam-diam gagal karena cache).
 
-const CACHE_NAME = 'kasir-offline-v9';
+const CACHE_NAME = 'kasir-offline-v10';
 
 const APP_SHELL = [
     './',
